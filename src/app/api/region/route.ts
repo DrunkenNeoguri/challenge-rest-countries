6db967c1866e49ae62466652fea4962e0a680c35
drunken_neoguri <developneoguri@gmail.com>
2024-08-22 20:18:21 +0900

    [Fix] 국가 리스트 api에서 대륙과 검색어에 따라 리스트 필터링 되게 수정

--- src/app/api/region/route.ts
+++ src/app/api/region/route.ts
@@ -1,7 +1,44 @@
 import { api } from "@/services/api";
 
-export async function GET() {
-  const res = await api.get("data.json").json<RegionType[]>();
+export async function GET(req: Request) {
+  const res = await api.get("data.json").json<NationType[]>();
 
-  return Response.json(res);
+  const { searchParams } = new URL(req.url);
+  const keyword = searchParams.get("keyword")?.trim().toLowerCase();
+  const region = searchParams.get("region")?.trim();
+
+  const result = res.filter((data) => {
+    if (keyword === "" && region === "") return true;
+
+    const checkKeyword = keyword
+      ? data.name.toLowerCase().includes(keyword)
+      : true;
+
+    const checkRegion = region ? data.region.includes(region) : true;
+
+    return checkKeyword && checkRegion;
+  });
+
+  return Response.json(result);
 }
+
+// export async function POST(req: Request) {
+//   const res = await api.get("data.json").json<NationType[]>();
+//   const formData = await req.formData();
+
+//   const keyword = formData.get("keyword")?.toString().toLowerCase();
+//   const region = formData.get("region")?.toString();
+
+//   const result = res.filter((data) => {
+//     if (!keyword && !region) return true;
+
+//     const checkKeyword = keyword
+//       ? data.name.toLowerCase().includes(keyword)
+//       : true;
+
+//     const checkRegion = region ? data.region.includes(region) : true;
+
+//     return checkKeyword && checkRegion;
+//   });
+//   return Response.json(result);
+// }
