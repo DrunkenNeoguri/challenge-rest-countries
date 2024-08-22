d0ccc22529b37f35dcea8f8f96a129c79ee5ca54
drunken_neoguri <developneoguri@gmail.com>
2024-08-22 20:27:39 +0900

    [Fix] 검색어 유무에 따라 searchParams가 변동될 수 있도록 검색창 로직 수정

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { DarkSearchIcon, LightSearchIcon } from "@/assets/icons";
-import { Box, TextField } from "@radix-ui/themes";
+import { TextField } from "@radix-ui/themes";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 type SearchBarPropType = {
   darkMode: boolean;
@@ -8,12 +10,44 @@ type SearchBarPropType = {
 
 export default function SearchBar(props: SearchBarPropType) {
   const { darkMode } = props;
+  const [keyword, setKeyword] = useState<string>();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleSetKeyword = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  };
+
+  const handleSearch = (e: FormEvent) => {
+    e.preventDefault();
+    const newParams = new URLSearchParams(searchParams);
+    if (!keyword || keyword.trim() === "") {
+      newParams.delete("keyword");
+      if (newParams.size === 0) {
+        return router.push(`${pathname}`);
+      } else {
+        return router.push(`${pathname}?${newParams.toString()}`);
+      }
+    } else {
+      newParams.set("keyword", keyword);
+      return router.push(`${pathname}?${newParams.toString()}`);
+    }
+  };
 
   return (
-    <TextField.Root placeholder="Search for a country..." size="3">
-      <TextField.Slot>
-        {!darkMode ? <LightSearchIcon /> : <DarkSearchIcon />}
-      </TextField.Slot>
-    </TextField.Root>
+    <form onSubmit={handleSearch}>
+      <TextField.Root
+        placeholder="Search for a country..."
+        style={{ width: "480px", height: "56px" }}
+        id="keyword"
+        value={keyword}
+        onChange={handleSetKeyword}
+      >
+        <TextField.Slot style={{ paddingLeft: "32px", paddingRight: "24px" }}>
+          {!darkMode ? <LightSearchIcon /> : <DarkSearchIcon />}
+        </TextField.Slot>
+      </TextField.Root>
+    </form>
   );
 }
