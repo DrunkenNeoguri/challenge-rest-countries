013207cdf73aee3a1f0c2f7f7fb20789322b28b1
drunken_neoguri <developneoguri@gmail.com>
2024-08-23 19:35:50 +0900

    [Fix] 국가 상세 정보에 받아온 데이터가 반영되도록 적용

--- src/app/nation/detail/_components/detail-section.tsx
+++ src/app/nation/detail/_components/detail-section.tsx
@@ -1,59 +1,104 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Section,
-  Strong,
-  Text,
-} from "@radix-ui/themes";
+"use client";
+import { Button, Flex, Heading, Section, Strong, Text } from "@radix-ui/themes";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function DetailSection(props: NationType) {
+  const {
+    name,
+    nativeName,
+    population,
+    region,
+    subregion,
+    capital,
+    topLevelDomain,
+    currencies,
+    languages,
+    borders,
+    flag,
+  } = props;
+
+  const router = useRouter();
+
+  const handleMoveToBack = () => {
+    return router.push("/nation");
+  };
+
   return (
-    <Section>
-      <Flex direction="column">
-        <Button>Back</Button>
-        <Flex>
-          <Box>Image</Box>
-          <Flex direction="column">
-            <Heading>Nation Name</Heading>
-            <Box>
+    <Section p="4">
+      <Flex justify="center" align="center">
+        <Flex direction="column" width="1440px">
+          <Button
+            size="3"
+            style={{ width: "360px", margin: "80px 0" }}
+            type="button"
+            onClick={handleMoveToBack}
+          >
+            Back
+          </Button>
+
+          <Flex justify="between">
+            <Image
+              alt={`${name}'s flag`}
+              src={flag}
+              width={560}
+              height={400}
+              style={{
+                objectFit: "cover",
+              }}
+            />
+            <Flex direction="column" gap="36px">
+              <Heading>{name}</Heading>
               <Flex>
-                <Text>
-                  <Strong>Native Name:</Strong>
-                </Text>
-                <Text>
-                  <Strong>Popluation: </Strong>
-                </Text>
-                <Text>
-                  <Strong>Region:</Strong>
-                </Text>
-                <Text>
-                  <Strong>Sub Region: </Strong>
-                </Text>
-                <Text>
-                  <Strong>Capital: </Strong>
-                </Text>
+                <Flex direction="column" gap="12px">
+                  <Text>
+                    <Strong>Native Name:</Strong>
+                    {nativeName}
+                  </Text>
+                  <Text>
+                    <Strong>Popluation: </Strong>
+                    {population}
+                  </Text>
+                  <Text>
+                    <Strong>Region:</Strong>
+                    {region}
+                  </Text>
+                  <Text>
+                    <Strong>Sub Region: </Strong>
+                    {subregion}
+                  </Text>
+                  <Text>
+                    <Strong>Capital: </Strong>
+                    {capital}
+                  </Text>
+                </Flex>
+                <Flex direction="column" gap="12px">
+                  <Text>
+                    <Strong>Top Level Domain:</Strong>
+                    {topLevelDomain}
+                  </Text>
+                  <Text>
+                    <Strong>Currencies: </Strong>
+                    {currencies?.map((currency) => {
+                      return <Text key={currency.code}>{currency.name}</Text>;
+                    })}
+                  </Text>
+                  <Text>
+                    <Strong>Languages: </Strong>
+                    {languages?.map((language) => {
+                      return <Text key={language.name}>{language.name}</Text>;
+                    })}
+                  </Text>
+                </Flex>
               </Flex>
               <Flex>
-                <Text>
-                  <Strong>Top Level Domain:</Strong>
-                </Text>
-                <Text>
-                  <Strong>Currencies: </Strong>
-                </Text>
-                <Text>
-                  <Strong>Languages: </Strong>
+                <Text my="32px">
+                  <Strong>Border Countries:</Strong>
+                  {borders?.map((border) => {
+                    return <Text key={border}>{border}</Text>;
+                  })}
                 </Text>
               </Flex>
-            </Box>
-            <Flex>
-              <Text>
-                <Strong>Border Countries:</Strong>
-              </Text>
-              <Box>A</Box>
-              <Box>B</Box>
-              <Box>C</Box>
             </Flex>
           </Flex>
         </Flex>
